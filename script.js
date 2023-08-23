1dd967299606b2572b5c73901e6878627ec56b07
AsatryanHov <111971218+AsatryanHov@users.noreply.github.com>
2023-08-23 20:17:23 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const targetElement = document.querySelector('.wrapper');
+const wrapper = document.querySelector('.wrapper');
 
 function resize() {
-  targetElement.style.width = window.innerWidth - 20 + 'px';
-  targetElement.style.height = window.innerHeight - 20 + 'px';
+  const mainElement = document.documentElement;
+  const mainElementWidth = mainElement.clientWidth;
+  const mainElementHeight = mainElement.clientHeight;
+
+  wrapper.style.width = mainElementWidth + 'px';
+  wrapper.style.height = mainElementHeight + 'px';
 }
 
 window.addEventListener('resize', resize);
